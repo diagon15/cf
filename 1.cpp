c932e4fc0a65c6931e207b48e1b17e7059060e72
diagon15 <102753220+diagon15@users.noreply.github.com>
2022-04-07 12:16:42 +0530

    solved dp on matrix in 2.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,116 +1,64 @@
-// problem link:
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define fastio    ios_base::sync_with_stdio(false);
-#define endl "\n";
 
-void yes(){ cout<<"YES"<<"\n"; return ;}
-void no(){ cout<<"NO"<<"\n"; return ;}
-template<typename T> void pnl(T a){ cout<<a<<"\n"; return;}
+const int INF = 1e9 + 7;
+const int H = 400;
+const int W = 400;
+int arr[H][W];
+int sums[H][W + 1];
 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 
-void _print(int x) { cout<<x;}
-void _print(long long x) { cout<<x;}
-void _print(char x) { cout<<x;}
-void _print(string x) { cout<<x;}
-void _print(bool x) { cout<<x;}
-void _print(size_t x) { cout<<x;}
+    int t;
+    cin >> t;
+    for (int ti = 0; ti < t; ++ti)
+    {
+        int h, w;
+        cin >> h >> w;
 
-void _print(pair<int,int> x) { _print("{"); _print(x.first); _print(","); _print(x.second); _print("}\n"); }
-void _print(pair<long long,long long> x) { _print("{"); _print(x.first); _print(","); _print(x.second); _print("}\n"); }
-void _print(pair<string,string> x) { _print("{"); _print(x.first); _print(","); _print(x.second); _print("}\n"); }
-
-template<class T> void _print(vector<T> v){    cout<< "[";     for(T i: v) _print(i), _print(' ');      cout<<"]";      }
-template<class T> void _print(set<T> v){    cout<< "[";     for(T i: v) _print(i), _print(' ');      cout<<"]";      }
-template<class T> void _print(multiset<T> v){    cout<< "[";     for(T i: v) _print(i), _print(' ');      cout<<"]";      }
-
-
-#define debug(x)    cout<<#x<<" "; (_print(x)); cout<<"\n";
-// #define debug(x)
-
-
-void testcase(int test){ // testcasesid
-
-    int n;
-    cin >> n;
-    ll x, y, u, v;
-    map<pair<ll,ll>, ll>mp;
-    ll pos = 0, neg = 0, pos2 = 0, neg2 = 0;
-    for(int i = 0; i < n; ++ i){
-        cin>> x >> y >> u >> v;
-        ll dy = v - y;
-        ll dx = u - x;
-        // int s1 = dy/abs(dy), s2 = dx/abs(dx);
-        // dy =abs(dy);
-        // dx =abs(dx);
-        // cout << "{"<< dx <<","<<dy<< "}\n";
-        ll g = abs(__gcd(dx, dy));
-        dy/= g;
-        dx/= g;
-        // debug(g);
-        // cout << "{"<< dx <<","<<dy<< "}\n";
-        // int sign = s1*s2;
-        // ++ mp[{sign*dx, dy}];
-        if(dx == 0){
-            if(dy > 0) ++pos;
-            else ++neg;
+        for (int y = 0; y < h; ++ y)
+        {
+            string str;
+            cin >> str;
+            for (int x = 0; x < w; ++x)
+                arr[y][x] = (str[x] == '1');
+            sums[y][0] = 0;
+            for (int x = 1; x <= w; ++x)
+                sums[y][x] = sums[y][x - 1] + arr[y][x - 1];
         }
-        else if(dy == 0){
-            if(dx > 0) ++pos2;
-            else ++ neg2;
-        }
-        else
-            ++ mp[{dx, dy}];
-    }
 
-    string encrypt(string word1) {
-        int n = word1.size();
-        string res = "";
-        for(int i = 0; i < n; ++ i){
-            if(mp.find(word1[i]) != mp.end()){
-                res += values[mp[word1[i]]];
+        int res = INF;
+        for (int x0 = 0; x0 < w; ++x0)
+        {
+            for (int x1 = x0 + 3; x1 < w; ++x1)
+            {
+                int len = x1 - x0 - 1;
+
+                array<int, 4> min_cost;
+                min_cost[0] = INF;
+                min_cost[1] = INF;
+                min_cost[2] = INF;
+                min_cost[3] = INF;
+
+                for (int y1 = 0; y1 < h; ++y1)
+                {
+                    int cou = sums[y1][x1] - sums[y1][x0 + 1];
+                    int empt = len - cou;
+                    int sd = 2 - arr[y1][x0] - arr[y1][x1];
+                    int off = min_cost[3] + empt;
+
+                    res = min(res, min_cost[3] + empt);
+                    min_cost[3] = cou + sd + min(min_cost[3], min_cost[2]);
+                    min_cost[2] = cou + sd + min_cost[1];
+                    min_cost[1] = cou + sd + min_cost[0];
+                    min_cost[0] = empt;
+                }
             }
         }
-        return res;
+        cout << res << '\n';
     }
-
-    // int decrypt(string word2, int index = 0, Trie* p = NULL) {
-    int decrypt(string word2) {
-    /*
-        if(index == 0) p = t;
-        if(index >= word2.size() and p->endofstr == true) return 1;
-        if(index >= word2.size()) return 0;
-        if(p == NULL) return 0;
-        int res = 0;
-        string str = word2.substr(index, 2);
-        for(auto &u: mp2[str]){
-            int idx = u - 'a';
-            if(p->next[idx] != NULL){
-                res += decrypt(word2, index + 2, p-> next[idx]);
-            }
-        }
-    }
-    ans += pos*neg + pos2 * neg2;
-    cout << ans << endl;
-
-    return;
 }
-
-
-int32_t main(){
-    fastio;
-    int test=1,z=1;
-    cin>>test;
-    while(z<=test){
-        testcase(z); z++;
-    }
-    return 0;
-}
-/*
-for std::lcm use -std=c++17 to compile locally
-
-g++ *.cpp > log.txt 2>&1
-
-topics:
-*/
--- 2.cpp
+++ 2.cpp
@@ -1,56 +1,143 @@
+// problem link:https://codeforces.com/problemset/problem/1580/A
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define fastio    ios_base::sync_with_stdio(false);
+#define endl "\n";
 
-#define ll long long
-class Solution {
-public:
-    long long numberOfWays1(string s) {
-        int n = s.length();
-        ll dp[4][n + 1], dp2[4][n + 1];
-        for(int i = 0; i <= n; ++ i) dp[0][i] = 1, dp2[0][i] = 1;
-        for(int i = 0; i < 4; ++ i) dp[i][0] = 1, dp2[i][0] = 1;
-
-        string str1 = "$010", str2 = "$101";
-        reverse(s.begin(), s.end());
-        s+="$";
-        reverse(s.begin(), s.end());
+void yes(){ cout<<"YES"<<"\n"; return ;}
+void no(){ cout<<"NO"<<"\n"; return ;}
+template<typename T> void pnl(T a){ cout<<a<<"\n"; return;}
 
 
+void _print(int x) { cout<<x;}
+void _print(long long x) { cout<<x;}
+void _print(char x) { cout<<x;}
+void _print(string x) { cout<<x;}
+void _print(bool x) { cout<<x;}
+void _print(size_t x) { cout<<x;}
 
-        for(int i = 1; i < 4; ++ i){
-            for(int j = 1; j <= n; ++ j){
-                if(str1[i] == s[j]){
-                    dp[i][j] = dp[i - 1][j - 1];
-                }
-                else {
-                    dp[i][j] = dp[i - 1][j] +  dp[i][j - 1];
-                }
+void _print(pair<int,int> x) { _print("{"); _print(x.first); _print(","); _print(x.second); _print("}\n"); }
+void _print(pair<long long,long long> x) { _print("{"); _print(x.first); _print(","); _print(x.second); _print("}\n"); }
+void _print(pair<string,string> x) { _print("{"); _print(x.first); _print(","); _print(x.second); _print("}\n"); }
 
-                if(str2[i] == s[j]){
-                    dp2[i][j] = dp2[i - 1][j - 1];
-                }
-                else {
-                    dp2[i][j] = dp2[i - 1][j] + dp2[i][j - 1];
-                }
+template<class T> void _print(vector<T> v){    cout<< "[";     for(T i: v) _print(i), _print(' ');      cout<<"]";      }
+template<class T> void _print(set<T> v){    cout<< "[";     for(T i: v) _print(i), _print(' ');      cout<<"]";      }
+template<class T> void _print(multiset<T> v){    cout<< "[";     for(T i: v) _print(i), _print(' ');      cout<<"]";      }
 
+
+#define debug(x)    cout<<#x<<" "; (_print(x)); cout<<"\n";
+// #define debug(x)
+
+const int N = 401;
+vector<vector<int>> arr(N, vector<int>(N, 0));
+vector<vector<int>> sums(N, vector<int>(N, 0));
+string s[N];
+
+int num_1(int y1, int x1, int y2, int x2){
+    // cout << y1 << " " << x1 << " " << y2 << " " << x2 << endl;
+    return sums[y2][x2] - sums[y2][x1 - 1] - sums[y1 - 1][x2] + sums[y1 - 1][x1 - 1];
+}
+
+int num_0(int y1, int x1, int y2, int x2){
+    return (y2 - y1 + 1) * (x2 - x1 + 1) - num_1(y1, x1, y2, x2);
+}
+void pit(){
+    for(int i = 0; i < sums.size(); ++ i){
+        for(int j = 0; j < sums.back().size(); ++ j){
+            cout << sums[i][j] ;
+            cout <<"\t";
+        }
+        cout << "\n";
+    }
+}
+void pit2(vector<vector<int>>& arr){
+    for(int i = 0; i < arr.size(); ++ i){
+        for(int j = 0; j < arr.back().size(); ++ j){
+            cout << arr[i][j] ;
+            // cout <<"\t";
+        }
+        cout << "\n";
+    }
+}
+
+#define SOME_MAX_VAL 160005
+void testcase(int test){ // testcasesid
+
+    int n, m;
+    cin >> n >> m;
+
+    for(int i = 1; i <= n; ++ i){
+        cin >> s[i];
+    }
+
+    sums = arr;
+
+    for(int i = 1; i <= n; ++ i){
+        for(int j = 1; j <= m; ++ j){
+            arr[i][j] = s[i][j - 1] == '1';
+        }
+    }
+
+    for(int i = 1; i <= n; ++ i){
+        for(int j = 1; j <= m; ++ j){
+            sums[i][j] = sums[i - 1][j] + sums[i][j - 1] - sums[i - 1][j - 1] + arr[i][j];
+        }
+    }
+    // pit();
+    // pit2(arr);
+    ll ans = SOME_MAX_VAL;
+    for(int x1 = 1; x1 <= m; ++ x1){
+        for(int x2 = x1 + 3; x2 <= m; ++ x2){
+            ll prev_val = SOME_MAX_VAL;
+            for(int y = 5; y <= n; ++ y){
+                // case1: new reactangle of height = 5
+                ll leftvertical = num_0(y - 3, x1, y - 1, x1);
+                ll rightvertical = num_0(y - 3, x2, y - 1, x2);
+                ll upperhorizontal = num_0(y - 4, x1 + 1, y - 4, x2 - 1);
+                ll lowerhorizontal = num_0(y, x1 + 1, y, x2 - 1);
+                ll innerbox = num_1(y - 3, x1 + 1, y - 1, x2 - 1);
+                ll case1cost = leftvertical + rightvertical + upperhorizontal + lowerhorizontal + innerbox;
+
+                // case2: extend the length by 1 of prev base
+                // ll case2cost = prev_val - num_0(y - 1, x1 + 1, y - 1, x2 - 1) + num_0(y - 1, x1, y - 1, x1)
+                // + num_0(y - 1, x2, y - 1, x2) + num_1(y, x1 + 1, y, x2 - 1) + num_0(y, x1 + 1, y, x2 - 1);
+                ll case2cost = prev_val - num_0(y - 1, x1 + 1, y - 1, x2 - 1) + num_1(y - 1, x1 + 1, y - 1, x2 - 1)
+                + num_0(y - 1, x1, y - 1, x1)
+                + num_0(y - 1, x2, y - 1, x2)
+                + num_0(y, x1 + 1, y, x2 - 1);
+                // + num_1(y, x1 + 1, y, x2 - 1)
+
+                ll curr_cost = min(case1cost, case2cost);
+                ans = min(curr_cost, ans);
+                prev_val = curr_cost;
+                // debug(innerbox);
+                // cout << case1cost << " "<< case2cost << " " << y << " " << x1 << " " << x2 <<endl;
+                // debug(case1cost);
+                // debug(ans);
             }
         }
-
-        cout << dp[3][n] << endl;
-        cout << dp2[3][n] << endl;
-        return dp[3][n] + dp2[3][n];
-
     }
 
-    ll solve(string &s, string str, vector<vector<int>>&dp){
-        int n =s.size(), m= str.size();
-        if(m == 0) return 1LL;
-        if(n == 0) return 0;
+    cout << ans << endl;
+
+    return;
+}
 
 
-        if(s.back() == dp)
+int32_t main(){
+    fastio;
+    int test=1,z=1;
+    cin>>test;
+    while(z<=test){
+        testcase(z); z++;
     }
+    return 0;
+}
+/*
+for std::lcm use -std=c++17 to compile locally
 
-    long long numberOfWays(string s) {
-        vector<vector<int>>dp(n,vector<int>(3,-1));
-        solve(s, "101",dp);
-    }
-};
+g++ *.cpp > log.txt 2>&1
+
+topics:
+*/
