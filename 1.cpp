8c32e36b5dbaacca4ef499586e023e68651fd9c0
diagon15 <siddharthmagadum15@gmail.com>
2022-07-04 20:01:57 +0530

    precontest

--- 1.cpp
+++ 1.cpp
@@ -1,79 +1,139 @@
-// { Driver Code Starts
+// problem link:
 #include <bits/stdc++.h>
 using namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+
+// template <class T>
+// using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+using ll = long long;
+#define fastio    ios_base::sync_with_stdio(false);
+#define endl "\n"
+#define dbg cerr
+
+void yes() { cout << "YES" << "\n"; return; }
+void no() { cout << "NO" << "\n"; return; }
+template<typename T> void pnl(T a) { cout << a << "\n"; return; }
+// std::apply([](auto&&... args) {((cout << args <<" "), ...); }, tp); // tuple print
+
+void _print(int x) { dbg << x; }
+void _print(long long x) { dbg << x; }
+void _print(char x) { dbg << x; }
+void _print(string x) { dbg << x; }
+void _print(bool x) { dbg << x; }
+void _print(size_t x) { dbg << x; }
+
+void _print(pair<int, int> x) { dbg << x.first << "," << x.second; }
+// void _print(pair<int,int> x) { _print("{"); _print(x.first); _print(","); _print(x.second); _print("}\n"); }
+void _print(pair<long long, long long> x) { _print("{"); _print(x.first); _print(","); _print(x.second); _print("}\n"); }
+void _print(pair<string, string> x) { _print("{"); _print(x.first); _print(","); _print(x.second); _print("}\n"); }
+
+template<class T> void _print(vector<T> v) { dbg << "[";     for (T i : v) _print(i), _print(' ');      dbg << "]"; }
+template<class T> void _print(set<T> v) { dbg << "[";     for (T i : v) _print(i), _print(' ');      dbg << "]"; }
+template<class T> void _print(multiset<T> v) { dbg << "[";     for (T i : v) _print(i), _print(' ');      dbg << "]"; }
 
 
-// } Driver Code Ends
+#ifndef ONLINE_JUDGE
+#define log(x)    dbg<<#x<<" = "; (_print(x)); dbg<<"\n";
+#else
+#  define log(x) 0
+#endif
 
 
-class Solution
-{
-public:
-    //Function to find the largest number after k swaps.
-    string findMaximumNum(string str, int k)
-    {
-        // code here.
-        // priority_queue<pair<char, int>> q;
-        set<pair<int, int>>st1;
-        set<pair<int, int>>st2;
-
-        for (int i = 0; i < str.size(); ++i)
-            st1.insert(make_pair(str[i], -i)),
-            st2.insert(make_pair(str[i], i));
+template<typename T>
+void p2darr(T* arr, int n, int m) { for (int i = 0; i < n; ++i) { for (int j = 0; j < m; ++j) cout << arr[i][j] << " "; cout << "\n"; } }
+template<typename T>
+void pvpair(T& vp) { for (int i = 0; i < vp.size(); ++i) cout << '{' << vp[i].first << ", " << vp[i].second << '}' << endl; }
 
 
-        for (int i = 0; i < str.size() and k > 0; ++i) {
-            // while (not q.empty() and -q.top().second < i) q.pop();
-            while(not st1.empty() and - (*--st1.end()).second < i){
-                st2.erase(*--st1.end());
-                st1.erase(--st1.end());
-            }
+void testcase(int test) { // testcasesid
 
-            if(str[i] == (*--st1.end()).first){
-                st1.erase(make_pair(str[i], i));
-                st2.erase(make_pair(str[i], i));
-            }
-            else {
-                cout << i << " " << (*--st1.end()).first << (*--st1.end()).second << endl;
-                int digit, idx;
-                if(str[i] < (*--st1.end()).first){
-                    digit = (*--st2.end()).first;
-                    idx = (*--st2.end()).second;
-                }
-                else{
-                    // break;
-                    continue;
-                    digit = (*--st1.end()).first;
-                    idx = - (*--st1.end()).second;
-                }
-                --k;
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<vector<pair<int, int>>>g(n, vector<pair<int, int>>());
+    map<pair<int, int>, int>getid;
+    for(int i = 0; i < m; ++ i){
+        int u, v, w;
+        cin >> u >> v >> w;
+        -- u, -- v;
+        g[u].push_back(make_pair(v, w));
+        g[v].push_back(make_pair(u, w));
+        getid[{u, v}] = i + 1;
+        getid[{v, u}] = i + 1;
+    }
 
-                st1.erase({str[i], i});
-                st2.erase({str[idx], idx});
+    int dist[n];
+    fill(dist, dist + n, n + 1);
+    dist[0] = 0;
+    priority_queue<array<int, 3>>pq;
+    pq.push({0, 0, 0});
+    // <-weight, -edges_encountered, node>
 
-                swap(str[i], str[idx]);
-                st2.insert({str[idx], idx});
+    vector<pair<int, int>>par(n);
+
+    while(not pq.empty()){
+        auto arr = pq.top();
+        pq.pop();
+        int edges_enc = arr[1];
+        int node = arr[2];
+
+        for(auto nxt: g[node]){
+            int dis = dist[node] + nxt.second;
+            if(dist[nxt.first] > dis){
+                dist[nxt.first] = dis;
+                pq.push({- dis, edges_enc - 1, nxt.first});
+                par[nxt.first] = make_pair(1-edges_enc, node);
             }
         }
-
-        return str;
     }
-};
 
-// { Driver Code Starts.
+    vector<array<int, 3>>shortlisted;
+    for(int i = 1; i < n; ++ i){
+        shortlisted.push_back({par[i].first, i, par[i].second});
+    }
 
-int main()
-{
-    int t, k;
-    string str;
+    sort(shortlisted.begin(), shortlisted.end());
 
-    // cin >> t;
-    // while (t--)
+    while(shortlisted.size() > k) shortlisted.pop_back();
+
+    cout << shortlisted.size() << endl;
+
+    for(int i = 0; i < shortlisted.size(); ++ i)
     {
-        cin >> k >> str;
-        Solution ob;
-        cout << ob.findMaximumNum(str, k) << endl;
+        int u = shortlisted[i][1];
+        int v = shortlisted[i][2];
+        cout << getid[{u, v}] << " ";
+    }
+
+
+
+
+    return;
+}
+
+
+
+int32_t main() {
+    fastio;
+    int test = 1, z = 1;
+    // cin >> test;
+    while (z <= test) {
+        testcase(z); z++;
     }
     return 0;
 }
-// } Driver Code Ends
+/*
+for std::lcm use -std=c++17 to compile locally
+
+g++ *.cpp > log.txt 2>&1
+
+topics:
+
+------------ TEEEPS --------------------
+If you have an observation, complement of that observation can be a better observation for code!!
+
+in dp, try to recognise the number of DIFFERENT states first. else your going wrong way!
+
+*/
